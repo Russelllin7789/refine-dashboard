5699ffcb1ebe885264f8982ffbe7bbc0c2316171
Russell Lin <superbr7789@gmail.com>
2024-06-18 08:33:39 +0800

    fulfill the custom-avatar section

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,9 +1,26 @@
-import { Avatar as AntdAvatar } from "antd"
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
 
-const CustomAvatar = () => {
+type Props = AvatarProps & {
+  name?: string;
+};
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
   return (
-    <AntdAvatar>JM</AntdAvatar>
-  )
-}
+    <AntdAvatar
+      alt={name}
+      size="small"
+      style={{
+        backgroundColor: "#87d068",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        ...style,
+      }}
+      {...rest}
+    >
+      {name}
+    </AntdAvatar>
+  );
+};
 
-export default CustomAvatar
+export default CustomAvatar;

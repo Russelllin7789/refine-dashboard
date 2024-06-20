ba5d47c41a445de4c3d23603f06308b7ff6a222a
Russell Lin <superbr7789@gmail.com>
2024-06-20 21:36:23 +0800

    add a sticky header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,25 @@
-import CurrentUser from "./current-user"
+import { Layout, Space } from "antd";
+import CurrentUser from "./current-user";
 
 const Header = () => {
-  return (
-    <div>
-        <CurrentUser />
-    </div>
-  )
-}
+  const headerStyles: React.CSSProperties = {
+    background: "#fff",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "0 24px",
+    position: "sticky",
+    top: 0,
+    zIndex: 999,
+  };
 
-export default Header
+  return (
+    <Layout.Header style={headerStyles}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
+  );
+};
+
+export default Header;

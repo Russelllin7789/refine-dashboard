92b3e34381c549bec3c2ff700fd29dc047d47ca5
Russell Lin <superbr7789@gmail.com>
2024-06-21 08:43:48 +0800

    add loading state within upcoming event table

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import { CalendarOutlined } from "@ant-design/icons";
+import { Card, List } from "antd";
+import { Text } from "../text";
 
 const UpcomingEvents = () => {
-  return <div>UpcomingEvents</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <Card
+      style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "0 1rem" }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <CalendarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.7rem" }}>
+            Upcoming Events
+          </Text>
+        </div>
+      }
+    >
+      {isLoading ? <List></List> : <List></List>}
+    </Card>
+  );
 };
 
 export default UpcomingEvents;

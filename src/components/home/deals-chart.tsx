324f2542619becc748c603175584e3976790c8b4
Russell Lin <superbr7789@gmail.com>
2024-06-22 23:09:15 +0800

    fulfill the deals-chart component

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,11 +1,59 @@
+import React from "react";
 import { DollarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
-import { Text } from "../text";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
 import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+
+import { Text } from "../text";
+import { mapDealsData } from "@/utilities/helpers";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { DashboardDealsChartQuery } from "@/graphql/types";
 
 const DealsChart = () => {
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: "dealStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["WON", "LOST"],
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  });
+
+  const dealDate = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
+  // further styling of the chart
   const config: AreaConfig = {
-    data: [],
+    data: dealDate,
+    xField: "timeText",
+    yField: "value",
+    isStack: false,
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v: string) => `${Number(v) / 1000}k`,
+      },
+    },
+    tooltip: {
+      formatter: (data) => ({
+        name: data.state,
+        value: `${Number(data.value) / 1000}k`,
+      }),
+    },
   };
 
   return (

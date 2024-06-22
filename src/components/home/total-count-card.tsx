6c366d36717eef69800142c610eadcedb37db2dc
Russell Lin <superbr7789@gmail.com>
2024-06-22 23:23:12 +0800

    add constants for whole project

---
+++ src/components/home/total-count-card.tsx
@@ -0,0 +1,31 @@
+import { Card } from "antd";
+import { icons } from "antd/es/image/PreviewGroup";
+import React from "react";
+
+const DashboardTotalCountCard = () => {
+  return (
+  <Card
+  style={{
+    height: "96px",
+    padding: 0
+}}
+styles={{
+    body: {
+      padding: "8px 8px 8px 12px",
+    },
+  }}
+  size="small"
+  >
+   <div
+    style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: '8px',
+        whiteSpace: 'nowrap'
+    }}
+   >
+    {icons[]}</div> 
+  </Card>);
+};
+
+export default DashboardTotalCountCard;

f8f094c111cef5128b4d610adbb4f0016e856e7e
Russell Lin <superbr7789@gmail.com>
2024-06-23 19:23:36 +0800

    fulfill the edit form section

--- src/pages/company/edit.tsx
+++ src/pages/company/edit.tsx
@@ -1,14 +1,37 @@
-import { Col, Form, Row } from "antd";
-import { Edit, useForm } from "@refinedev/antd";
-import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations";
-import CustomAvatar from "@/components/custom-avatar";
+import { Col, Form, Input, InputNumber, Row, Select } from "antd";
+import { Edit, useForm, useSelect } from "@refinedev/antd";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+
 import { getNameInitials } from "@/utilities";
+import { UsersSelectQuery } from "@/graphql/types";
+import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import { USERS_SELECT_QUERY } from "@/graphql/queries";
+import CustomAvatar from "@/components/custom-avatar";
+import SelectOptionWithAvatar from "@/components/select-option-with-avatar";
+import {
+  businessTypeOptions,
+  companySizeOptions,
+  industryOptions,
+} from "@/constants";
 
 export const EditPage = () => {
   const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
     redirect: false,
     meta: {
       gqlMutation: UPDATE_COMPANY_MUTATION,
+    },
+  });
+
+  const { selectProps, queryResult: queryResultUsers } = useSelect<
+    GetFieldsFromList<UsersSelectQuery>
+  >({
+    resource: "users",
+    optionLabel: "name",
+    pagination: {
+      mode: "off",
+    },
+    meta: {
+      gqlQuery: USERS_SELECT_QUERY,
     },
   });
 
@@ -34,6 +57,51 @@ export const EditPage = () => {
                   marginBottom: "24px",
                 }}
               />
+              <Form.Item
+                label="Sales owner"
+                name="salesOwnerId"
+                initialValue={formProps?.initialValues?.salesOwner?.id}
+              >
+                <Select
+                  placeholder="Please select a sales owner"
+                  {...selectProps}
+                  options={
+                    queryResultUsers.data?.data.map((user) => ({
+                      value: user.id,
+                      label: (
+                        <SelectOptionWithAvatar
+                          name={user.name}
+                          avatarUrl={user.avatarUrl ?? undefined}
+                        />
+                      ),
+                    })) ?? []
+                  }
+                />
+              </Form.Item>
+              <Form.Item>
+                <Select options={companySizeOptions} />
+              </Form.Item>
+              <Form.Item>
+                <InputNumber
+                  autoFocus
+                  addonBefore="$"
+                  min={0}
+                  placeholder="0,00"
+                />
+              </Form.Item>
+              <Form.Item label="Industry">
+                <Select options={industryOptions} />
+              </Form.Item>
+              <Form.Item label="Business type">
+                <Select options={businessTypeOptions} />
+              </Form.Item>
+              {/* {connect the value to the form input with the name} */}
+              <Form.Item label="Country" name="country">
+                <Input placeholder="Country" />
+              </Form.Item>
+              <Form.Item label="Website" name="website">
+                <Input placeholder="Website" />
+              </Form.Item>
             </Form>
           </Edit>
         </Col>

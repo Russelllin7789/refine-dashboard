4d53b7377dc12773e3d477653a5d6ea6f416d35f
Russell Lin <superbr7789@gmail.com>
2024-06-24 21:52:49 +0800

    add dnd library for the kanban board

--- src/components/tasks/kanban/board.tsx
+++ src/components/tasks/kanban/board.tsx
@@ -1,17 +1,33 @@
+import { DndContext } from "@dnd-kit/core";
 import React from "react";
 
-export const KanbanBoardContainer = () => {
+export const KanbanBoardContainer = ({ children }: React.PropsWithChildren) => {
   return (
-    <>
-      <div>KanbanBoard</div>
-    </>
+    <div
+      style={{
+        width: "calc(100% + 64px)",
+        // minus the height of the top nav bar
+        height: "calc(100vh - 64px)",
+        display: "flex",
+        justifyContent: "column",
+        margin: "-32px",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          padding: "32px",
+          overflow: "scroll",
+        }}
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
-export const KanbanBoard = () => {
-  return (
-    <>
-      <div>KanbanBoard</div>
-    </>
-  );
+export const KanbanBoard = ({ children }: React.PropsWithChildren) => {
+  return <DndContext>{children}</DndContext>;
 };
